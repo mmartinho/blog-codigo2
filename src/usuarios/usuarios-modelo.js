5c045eaa27ee0e41a80c8f0c33c197ddca57fda1
marcus.martinho <marcus.martinho@tjam.jus.br>
2021-04-09 18:59:10 -0400

    Capítulo 01: Protegendo as senhas no banco de dados - FINALIZADO

---
+++ src/usuarios/usuarios-modelo.js
@@ -0,0 +1,78 @@
+const usuariosDao = require('./usuarios-dao');
+const { InvalidArgumentError } = require('../erros');
+const validacoes = require('../validacoes-comuns');
+const bcrypt = require('bcrypt');
+
+class Usuario {
+  constructor(usuario) {
+    this.id = usuario.id;
+    this.nome = usuario.nome;
+    this.email = usuario.email;
+    this.senha = usuario.senhaHash;
+
+    this.valida();
+  }
+
+  async adiciona() {
+    if (await Usuario.buscaPorEmail(this.email)) {
+      throw new InvalidArgumentError('O usuário já existe!');
+    }
+
+    return usuariosDao.adiciona(this);
+  }
+
+  /**
+   * Novo método para validar e "hashear" a senha
+   * @param {*} senha 
+   */
+  async adicionaSenha(senha) {
+    validacoes.campoStringNaoNulo(senha, 'senha');
+    validacoes.campoTamanhoMinimo(senha, 'senha', 8);
+    validacoes.campoTamanhoMaximo(senha, 'senha', 64);    
+    this.senhaHash = await Usuario.gerarSenhaHash(senha);
+  }
+
+  valida() {
+    validacoes.campoStringNaoNulo(this.nome, 'nome');
+    validacoes.campoStringNaoNulo(this.email, 'email');
+  }
+
+  
+  async deleta() {
+    return usuariosDao.deleta(this);
+  }
+  
+  static async buscaPorId(id) {
+    const usuario = await usuariosDao.buscaPorId(id);
+    if (!usuario) {
+      return null;
+    }
+    
+    return new Usuario(usuario);
+  }
+  
+  static async buscaPorEmail(email) {
+    const usuario = await usuariosDao.buscaPorEmail(email);
+    if (!usuario) {
+      return null;
+    }
+    
+    return new Usuario(usuario);
+  }
+
+  static lista() {
+    return usuariosDao.lista();
+  }
+
+  /**
+   * Criptografa a senha original
+   * @param {*} senha 
+   * @returns 
+   */
+  static gerarSenhaHash(senha) {
+    const custoHash = 12;
+    return bcrypt.hash(senha, custoHash);
+  }
+}
+
+module.exports = Usuario;
